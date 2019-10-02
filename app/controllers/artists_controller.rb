c71a6119d6d765ff16397ef95f8bc552b452e37c
David Chiu <davidwchiu@gmail.com>
2019-10-02 00:35:33 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -11,7 +11,7 @@ class ArtistsController < ApplicationController
     if Preference.first_create_artists
       @artist = Artist.new
     else
-      redirect_to 
+      redirect_to artists
   end
 
   def create
